f40e93751505ffbf7a757fcc704484e1fd047189
Scully87 <scully87.ts@googlemail.com>
2014-11-28 13:57:39 +0000

    operators

---
+++ Operators.playground/section-1.swift
@@ -0,0 +1,12 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+let height = 12.0 // in feet
+let width = 10.0 // in feet
+
+let area = height * width
+
+// 1 sq. meter = 1 sq. foot / 10.764
+
+let areaInMeters = area / 10.764
